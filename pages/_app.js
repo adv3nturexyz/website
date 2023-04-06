78fe956ce36d9b3b801ce45455c60f2db3fffcfa
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-04-06 11:56:39 +0800

    fix: gtag implementation (not working?)
    https://nextjs.org/docs/messages/next-script-for-ga#using-gtagjs

--- pages/_app.js
+++ pages/_app.js
@@ -11,27 +11,24 @@ const MyApp = ({ Component, pageProps }) => (
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-K4TVEB7SLK"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-K4TVEB7SLK', {
-            page_path: window.location.pathname,
-          });
-          `,
-        }}
-      />
+      <div>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K4TVEB7SLK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-K4TVEB7SLK');
+          `}
+        </Script>
+      </div>
       {/* <!-- Mailchimp code snippet --> */}
-      <Script
+      {/* <Script
         id="mcjs"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
@@ -39,7 +36,7 @@ const MyApp = ({ Component, pageProps }) => (
           !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/5f31e382e95fa2e353915e688/31b74d347e4e4c5fa70983123.js");
           `,
         }}
-      />
+      /> */}
     </Head>
     <Component {...pageProps} />
   </>
